9fb08aae65309b408c5c883b40d1a9d50a395e22
Anton Drevin <a.drevin@mail.ru>
2025-05-26 00:30:46 +0300

    add def mask_account_card, def get_data

---
+++ src/widget.py
@@ -0,0 +1,25 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(name_card: str) -> str:
+    '''принимает катру/счет, возвращает тип карты/счет и замаскированый номер карты/счета'''
+    if "счет" in name_card.lower():
+        number_card = name_card[-20:]
+        mask_card = get_mask_account(number_card)
+        return f"счет {mask_card}"
+
+    else:
+        type_card = name_card[:-16]
+        number_card = name_card[-16:]
+        mask_card = get_mask_card_number(number_card)
+        return f"{type_card} {mask_card}"
+
+
+def get_data(time_card: str) -> str:
+    '''принимает системную дату/время, возвращает дату в формате ДД.ММ.ГГГГ'''
+    return time_card[8:10] + "." + time_card[5:7] + "." + time_card[:4]
+
+
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(get_data("2024-03-11T02:26:18.671407"))
