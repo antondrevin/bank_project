1640fe327192f13e369ca6a9b30ba37ead7fb285
Anton Drevin <a.drevin@mail.ru>
2025-05-26 16:14:45 +0300

    upgrade linters

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,7 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(name_card: str) -> str:
-    '''принимает катру/счет, возвращает тип карты/счет и замаскированый номер карты/счета'''
+    """принимает катру/счет, возвращает тип карты/счет и замаскированый номер карты/счета"""
     if "счет" in name_card.lower():
         number_card = name_card[-20:]
         mask_card = get_mask_account(number_card)
@@ -16,5 +16,5 @@ def mask_account_card(name_card: str) -> str:
 
 
 def get_data(time_card: str) -> str:
-    '''принимает системную дату/время, возвращает дату в формате ДД.ММ.ГГГГ'''
+    """принимает системную дату/время, возвращает дату в формате ДД.ММ.ГГГГ"""
     return time_card[8:10] + "." + time_card[5:7] + "." + time_card[:4]
