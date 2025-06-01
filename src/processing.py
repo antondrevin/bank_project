d1dd46d9843bbd9503fef6567b2e518066dae43f
Anton Drevin <a.drevin@mail.ru>
2025-06-01 19:13:53 +0400

    исправление ошибок mypy

--- src/processing.py
+++ src/processing.py
@@ -10,6 +10,6 @@ def filter_by_state(list_state: list[dict], key_state: str = "EXECUTED") -> list
 
 def sort_by_date(list_state: list[dict], sorting: bool = True) -> list[dict]:
     """функция сортирует по дате операции"""
-    sorted_state = sorted(list_state, key=lambda x: x.get("date"), reverse=sorting)
+    sorted_state = sorted(list_state, key=lambda x: x["date"], reverse=sorting)
 
     return sorted_state
